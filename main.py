1bd3f6a511af4efa31a4af21c1681ead3b426534
apurbzz1994 <akash95august@gmail.com>
2024-08-11 02:55:16 +1000

    add error handling for function load

--- main.py
+++ main.py
@@ -12,45 +12,48 @@ import traceback
 eel.init("web")
 
 
-
-#some global variables to keep track of modules getting imported (key refers to the module name)
+# some global variables to keep track of modules getting imported (key refers to the module name)
 imported_modules = {}
 
-#list of added tasks
+# list of added tasks
 all_tasks = []
 
-#define a stop event
+# define a stop event
 stop_event = threading.Event()
 scheduler_thread = None
-
 
 
 '''
 getting filenames for all python scripts in the directory
 '''
+
+
 def get_module_names():
     script_dir = './'
     all_modules = []
 
     if os.path.exists(script_dir):
-        #look for files with the .py extension
+        # look for files with the .py extension
         for each_file in os.listdir(script_dir):
             if each_file.endswith('.py') and each_file != 'main.py':
-                module_name = each_file.split('.')[0] #getting rid of the py file extension
+                # getting rid of the py file extension
+                module_name = each_file.split('.')[0]
                 all_modules.append(module_name)
     else:
         print('scripts folder not found. please create one and dump all your scripts. Empty list will be returned')
     return all_modules
 
 
-
 '''
 When passed a module reference, this will return a list of all functions
 within that module 
 '''
+
+
 def load_module_functions(module_ref):
     all_attr = dir(module_ref)
-    all_functions = [attr for attr in all_attr if inspect.isfunction(getattr(module_ref, attr))]
+    all_functions = [attr for attr in all_attr if inspect.isfunction(
+        getattr(module_ref, attr))]
     return all_functions
 
 
@@ -58,6 +61,8 @@ def load_module_functions(module_ref):
 This grabs all module names and sends over to the frontend 
 for populating the first dropdown
 '''
+
+
 @eel.expose
 def send_module_list():
     module_list = get_module_names()
@@ -74,35 +79,48 @@ creates a reference to the import and stores it in a global dictionary.
 
 Also sends over functions list to the frontend to render the second dropdown
 '''
+
+
 @eel.expose
 def send_functions_list(selected_module):
-    #create a record in the imported modules dict provided it doesn't already exist
-    if selected_module not in imported_modules:
-        imported_modules[selected_module] = importlib.import_module(selected_module)
+    module_func = None
+    try:
+        # create a record in the imported modules dict provided it doesn't already exist
+        if selected_module not in imported_modules:
+            imported_modules[selected_module] = importlib.import_module(
+                selected_module)
 
-    #loading functions associated with selected module
-    module_functions = load_module_functions(imported_modules[selected_module])
+        # loading functions associated with selected module
+        module_functions = load_module_functions(imported_modules[selected_module])
 
-    module_func = {
-        'functions': module_functions
-    }
+        module_func = {
+            'functions': module_functions,
+            'error': 'no'
+        }
+    except Exception as e:
+        error_traceback = traceback.format_exc()
+        error_message = f"{e}|{error_traceback}"
+        module_func = {
+             'functions': [],
+             'error': error_message
+        }
 
     to_render = json.dumps(module_func)
     return to_render
 
 
 def refresh_task_view(state):
-    #trigger a page refresh on some sort here
-    flipped_task_list = sorted(all_tasks, key=lambda x: x["set-time"], reverse=False)
-    #return the list with new tasks to JS
+    # trigger a page refresh on some sort here
+    flipped_task_list = sorted(
+        all_tasks, key=lambda x: x["set-time"], reverse=False)
+    # return the list with new tasks to JS
     to_render = json.dumps(flipped_task_list)
     eel.renderAddedTasks(to_render)
     eel.disableAllDeleteButtons(state)
 
 
-
 def main_thread():
-    #loading all the modules
+    # loading all the modules
     module_list_strings = get_module_names()
     modules = {}
     if len(module_list_strings) != 0:
@@ -110,16 +128,15 @@ def main_thread():
             modules[each] = importlib.import_module(each)
     else:
         print('No modules returned')
-    
 
-    #loading functions associated with the modules
+    # loading functions associated with the modules
     for each_module in module_list_strings:
         functions_within = load_module_functions(modules[each_module])
         print(functions_within)
 
 
-#add to a list of tasks
-@eel.expose 
+# add to a list of tasks
+@eel.expose
 def add_to_task_list(task_json):
     '''
     1. create an array called all_tasks
@@ -139,13 +156,13 @@ def add_to_task_list(task_json):
     '''
 
     task_to_add = json.loads(task_json)
-    #assign unique id to each task
+    # assign unique id to each task
     task_to_add['id'] = secrets.token_hex(4)
 
-    #adding a variable to track the number of times the task ran, starts with zero
+    # adding a variable to track the number of times the task ran, starts with zero
     task_to_add['freq'] = 0
 
-    #do come calculations and add a field to denote next run-time
+    # do come calculations and add a field to denote next run-time
     # current_time = datetime.datetime.now()
     # next_runtime = None
 
@@ -156,13 +173,14 @@ def add_to_task_list(task_json):
 
     task_to_add['next-runtime'] = "Not Running"
 
-    #add to all_tasks array
+    # add to all_tasks array
     all_tasks.append(task_to_add)
 
-    #sorting based on smallest time
-    flipped_task_list = sorted(all_tasks, key=lambda x: x["set-time"], reverse=False)
+    # sorting based on smallest time
+    flipped_task_list = sorted(
+        all_tasks, key=lambda x: x["set-time"], reverse=False)
 
-    #return the list with new tasks to JS
+    # return the list with new tasks to JS
     to_render = json.dumps(flipped_task_list)
     return to_render
 
@@ -173,36 +191,38 @@ def remove_from_task_list(task_id):
         if each_task['id'] == task_id:
             all_tasks.remove(each_task)
 
+
 '''
 function wrapper to keep track of function runs and manage UI updates
 '''
+
+
 def handle_function_run(f, task_id):
     try:
-        #try running this script
-       execution =  f() 
+        # try running this script
+        execution = f()
     except Exception:
         print('Script execution unsuccessful: ')
-        #write this to a file if script is unsuccessful
+        # write this to a file if script is unsuccessful
         traceback.print_exc()
     else:
-       #grab the dictionary from list of tasks, should only be one
-       task_matching_id = list(filter(lambda d: d['id'] == task_id, all_tasks))
-       #grab the frequency
-       new_freq = task_matching_id[0]['freq'] + 1
-       #update frequency
-       task_matching_id[0]['freq'] = new_freq
-       #determine the next runtime
-       #next_runtime = schedule.next_run(task_matching_id[0]['id'])
-       #update next runtime
-       #task_matching_id[0]['next-runtime'] = next_runtime.strftime('%d-%m-%Y_%H-%M-%S') 
-       #trigger a page refresh on some sort here
-       refresh_task_view('play')
+        # grab the dictionary from list of tasks, should only be one
+        task_matching_id = list(
+            filter(lambda d: d['id'] == task_id, all_tasks))
+        # grab the frequency
+        new_freq = task_matching_id[0]['freq'] + 1
+        # update frequency
+        task_matching_id[0]['freq'] = new_freq
+        # determine the next runtime
+        # next_runtime = schedule.next_run(task_matching_id[0]['id'])
+        # update next runtime
+        # task_matching_id[0]['next-runtime'] = next_runtime.strftime('%d-%m-%Y_%H-%M-%S')
+        # trigger a page refresh on some sort here
+        refresh_task_view('play')
 
 
-
-
-#schedules one task based on dict info
-#this is where the main shedule is mentioned
+# schedules one task based on dict info
+# this is where the main shedule is mentioned
 def define_scheduler(task):
     '''
     structure for a task
@@ -215,15 +235,15 @@ def define_scheduler(task):
     '''
     task_module = imported_modules[task['module']]
 
-    #grabbing the function reference 
+    # grabbing the function reference
     task_function = getattr(task_module, task['function'])
 
     if task['set-type'] == 'sec':
-        schedule.every(int(task['set-time'])).seconds.do(handle_function_run, f = task_function, task_id = task['id']).tag(task['id'])
+        schedule.every(int(task['set-time'])).seconds.do(handle_function_run,
+                                                         f=task_function, task_id=task['id']).tag(task['id'])
     else:
-        schedule.every(int(task['set-time'])).minutes.do(handle_function_run, f = task_function, task_id = task['id']).tag(task['id'])
-
-
+        schedule.every(int(task['set-time'])).minutes.do(handle_function_run,
+                                                         f=task_function, task_id=task['id']).tag(task['id'])
 
 
 def initial_next_task_runtime_update():
@@ -231,7 +251,7 @@ def initial_next_task_runtime_update():
         next_task_run = schedule.next_run(each_task['id'])
         each_task['next-runtime'] = next_task_run.strftime('%d/%m/%Y %H:%M:%S')
     refresh_task_view('play')
-   
+
 
 def reset_all_next_runtime():
     for each_task in all_tasks:
@@ -242,12 +262,14 @@ def reset_all_next_runtime():
 '''
 function to run scheduler with a thread event
 '''
+
+
 def run_schedule(stop_event):
     '''
     1. schedule.next_run() will grab the schedule object that is meant to run next
     2. you can grab more information regarding the next scheduled task from here, possibly tags as well
     3. trigger the JS function based on next runtime
-    
+
     '''
     next_run_time = None
     while not stop_event.is_set():
@@ -256,29 +278,32 @@ def run_schedule(stop_event):
         next_run = schedule.next_run()
         if next_run and next_run != next_run_time:
             next_run_time = next_run
-            print(f"Next scheduled job will run at {next_run.strftime('%Y-%m-%d %H:%M:%S')}")
+            print(f"Next scheduled job will run at {
+                  next_run.strftime('%Y-%m-%d %H:%M:%S')}")
         time.sleep(1)
-
 
 
 '''
 Use the scheduler library to schedule all tasks in the all_tasks list
 '''
+
+
 @eel.expose
 def trigger_schedule_tasks():
 
-    #set the thread event to false initially
+    # set the thread event to false initially
     global stop_event
     if stop_event.is_set():
         stop_event.clear()
 
-    #scheduling everything here
+    # scheduling everything here
     for each_task in all_tasks:
         define_scheduler(each_task)
 
-    #start the scheduler in a different thread
+    # start the scheduler in a different thread
     global scheduler_thread
-    scheduler_thread = threading.Thread(target=run_schedule, args=(stop_event,))
+    scheduler_thread = threading.Thread(
+        target=run_schedule, args=(stop_event,))
     scheduler_thread.start()
 
 
@@ -287,22 +312,21 @@ def trigger_task_halt():
     if scheduler_thread is not None:
         stop_event.set()
         scheduler_thread.join()
-        #clear all previous jobs
+        # clear all previous jobs
         schedule.clear()
         reset_all_next_runtime()
-        #clearing out the task list as well
+        # clearing out the task list as well
         all_tasks.clear()
         imported_modules.clear()
     else:
         all_tasks.clear()
         imported_modules.clear()
     print('Scheduler has stopped and has been cleared')
-       
-    
+
 
 def main():
     eel.start("index.html")
-            
+
 
 if __name__ == "__main__":
     main()
